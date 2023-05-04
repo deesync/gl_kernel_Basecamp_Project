9f283752cc6a7a5389384e4cba89132bb1fbd98a
Vlad Degtyarov <deesyync@gmail.com>
2023-05-04 20:25:13 +0000

    Display: Implement minimal display driver
    Implemented graphic display driver for SSD1306 OLED dot matrix
    with minimal functionality.
    
    Driver has an API calls with which you can:
     - print text with selected font
     - clear the screen
    
    Signed-off-by: Vlad Degtyarov <deesyync@gmail.com>

--- display/display_module.h
+++ display/display_module.h
@@ -3,6 +3,12 @@
 #ifndef __DISPLAY_MODULE_H__
 #define __DISPLAY_MODULE_H__
 
-extern int bc_display_data(char *str);
+#include "ssd1306.h"
+#include "display_fonts.h"
+
+#define MAX_STR_LEN 21
+
+extern int bc_display_clear(void);
+extern int bc_display_print(u8 offset, u8 line, const struct display_font_t *font, char *str);
 
 #endif // __DISPLAY_MODULE_H__
